b0149c6c1ae2024f6b1e2328819001caacdc404d
Sesilyo <c.sethleander@gmail.com>
2026-05-22 22:10:47 +0800

    dummy commit

--- api/getItems.php
+++ api/getItems.php
@@ -3,7 +3,7 @@
 
     require_once __DIR__ . '/../DBConnector.php';
 
-    $result = $conn -> query("  SELECT  i.item_id, i.item_name, i.item_status, i.price_per_hour,
+    $result = $conn -> query("  SELECT  i.item_id, i.item_name, i.item_status, i.price_pr_hr, i.image_path,
                                         c.category_name, u.first_name, u.last_name
                                 FROM item i
                                 JOIN category c ON i.category_id = c.category_id
