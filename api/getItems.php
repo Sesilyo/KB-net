887277d76a7055ed6ac209674fa16773279f4f77
e-rine <elmedalla@up.edu.ph>
2026-05-22 22:09:19 +0800

    getitems

--- api/getItems.php
+++ api/getItems.php
@@ -3,11 +3,11 @@
 
     require_once __DIR__ . '/../DBConnector.php';
 
-    $result = $conn -> query("  SELECT  i.item_id, i.item_name, i.item_status, i.price_per_hour,
+    $result = $conn -> query("  SELECT  i.item_id, i.item_name, i.item_status, i.price_pr_hr,
                                         c.category_name, u.first_name, u.last_name
                                 FROM item i
                                 JOIN category c ON i.category_id = c.category_id
-                                JOIN user u ON i.user_id = u.student_id
+                                JOIN user u ON i.lender_id = u.lender_id
     ");
 
     $items = [];
