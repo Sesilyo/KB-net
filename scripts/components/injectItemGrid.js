dec871bd19b96b047e2bb8a1e5064e9bbb4f4bf1
Sesilyo <c.sethleander@gmail.com>
2026-05-24 22:51:03 +0800

    polished browse stylings

--- scripts/components/injectItemGrid.js
+++ scripts/components/injectItemGrid.js
@@ -3,14 +3,21 @@
 function createItemCard(item) {
         return `
             <div class="item-card" data-id="${item.item_id}">
+                <span class="item-status ${item.item_status}">${item.item_status}</span>
+                <img alt="item img here">
                 <div class="item-info">
-                    <span class="item-category">${item.category_name}</span>
-                    <p class="item-name">${item.item_name}</p>
-                    <p class="item-lender">${item.first_name} ${item.last_name}</p>
+                    <div class="item-body">
+                        <div class="item-labels">
+                            <p class="item-name">${item.item_name}</p>
+                            <span class="item-category">${item.category_name}</span>
+                        </div>
+
+                        <span class="item-price">₱${parseFloat(item.price_pr_hr).toFixed(2)} / hr</span>
+                    </div>
                     
                     <div class="item-footer">
-                        <span class="item-price">₱${parseFloat(item.price_pr_hr).toFixed(2)} / hr</span>
-                        <span class="item-status ${item.item_status}">${item.item_status}</span>
+                        <p class="item-lender">${item.first_name} ${item.last_name}</p>
+                        <button class="borrow-btn">Borrow</button>
                     </div>
                 </div>
             </div>
